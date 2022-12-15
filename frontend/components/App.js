4cf47179710983139d6d9b8fb299d582e05cd7d5
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-15 11:59:15 -0700

    create App and Form components -- need to add onSubmit to Form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import Form from './Form';
 
 const URL = 'http://localhost:9000/api/todos'
 
+
+
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: ''
+    }
+    }
   render() {
-    return null
+    return (
+      <>
+        <div>
+          <h2>Todo List:</h2>
+          <ul>
+            <li>Walk the Dog</li>
+            <li>Learn React</li>
+            <li>Have Fun</li>
+          </ul>
+
+          <Form />
+        </div>
+        <br></br>
+        <div>
+          <button>Hide Complete</button>
+        </div>
+      </>
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+  onChange = (evt) => {
+  this.setState({
+    ...this.state,
+    input: evt.target.value
+  })
+  }
+  onSubmit = () => {
+    
+  }
   render() {
-    return null
+    return(
+      <form onSubmit={this.onSubmit}>
+        <input onChange={this.onChange} placeholder="Type Todo" />
+        <button>Submit</button>
+      </form>
+    )
   }
 }
